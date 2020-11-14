184534731765856f2b44a10a97d029b82ddb0240
Iroel Perez <iroel.perez@gmail.com>
2020-11-14 14:03:26 -0500

    Refactoring the StarMatch game.

---
+++ src/components/PlayNumber.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Color Theme
+export const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+export const PlayNumber = (props) => (
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
